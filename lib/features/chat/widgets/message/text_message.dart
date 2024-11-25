7d2deb040e8ac493aee3975e4e125fb7eb21f269
Nguyễn Ngọc Đức <106262560+ngocduc63@users.noreply.github.com>
2024-11-25 21:03:02 +0700

    fix width text mess

--- lib/features/chat/widgets/message/text_message.dart
+++ lib/features/chat/widgets/message/text_message.dart
@@ -2,7 +2,6 @@ import 'package:facebook/constants/app_colors.dart';
 import 'package:facebook/models/message_model.dart';
 import 'package:flutter/material.dart';
 
-
 class TextMessage extends StatelessWidget {
   const TextMessage({
     Key? key,
@@ -13,14 +12,20 @@ class TextMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20 * 0.75, vertical: 20 / 2),
       decoration: BoxDecoration(
-          color: AppColors.lightBlueColor.withOpacity(message.isSender() ? 1 : 0.08), borderRadius: BorderRadius.circular(30)),
-      child: Text(
-        message.data?.content ?? "",
-        style: TextStyle(color: message.isSender() ? Colors.white : Theme.of(context).textTheme.bodySmall?.color),
+        color: AppColors.lightBlueColor.withOpacity(message.isSender() ? 1 : 0.08),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: screenWidth * 0.6),
+        child: Text(
+          message.data?.content ?? "",
+          style: TextStyle(color: message.isSender() ? Colors.white : Theme.of(context).textTheme.bodySmall?.color),
+        ),
       ),
     );
   }
